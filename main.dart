eced3ad64644b5edef226ea6a9d04989cbfea66e
Vivek Sawji <viveksawji06@gmail.com>
2026-07-17 18:56:24 +0530

    Add async data fetching example in main.dart

---
+++ main.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+
+Future<void> getData() async {
+  print("Fetching Data...");
+
+  await Future.delayed(Duration(seconds: 2));
+
+  print("Data Loaded");
+}
+
+void main() async {
+  print("Start");
+
+  await getData();
+
+  print("End");
+}
